a1c69a8d4c60d63e3f05fad762792e4788ce77c8
Evan Colony <evancolo@umich.edu>
2018-10-31 16:58:39 -0400

    Testing VS git integration

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+/*
+ * Name: Evan Colony
+ * Date: 10/31/2018
+ * Description: The purpose of this is to calculate various probabilties given other specefic values
+ */
+
+int main() {
+
+
+    return 0;
+}
