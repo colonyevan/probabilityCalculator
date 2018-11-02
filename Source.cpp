34b6d7a6affdad3dfb49f006ec18751e7cd2ce63
evancolo <evancolo@umich.edu>
2018-11-01 22:25:15 -0400

    More Testing

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,18 @@
  * Description: The purpose of this is to calculate various probabilties given other specefic values
  */
 
+// Including header files
+
+// including libraries
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
 int main() {
+    cout << "This program is incredibly sensitive to input." << endl << endl
+        << "\t First, you MUST make sure the same event is spelled the same EVERYWHERE, I will not spellcheck!" << endl
+        << "\t Second, to indicate a conditional probabiltiy, use the character | (this character may not be used anywhere else)" << endl;
 
 
     return 0;
