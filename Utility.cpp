a66a8506452212b5ce768149f4d13ad74b6c5e69
evancolo <evancolo@umich.edu>
2018-11-01 22:52:04 -0400

    Inital architecture

---
+++ Utility.cpp
@@ -0,0 +1,15 @@
+#include "Utility.h"
+
+#include <iostream>
+
+using namespace std;
+
+void calcHeader() {
+    cout << "This program is incredibly sensitive to input." << endl << endl
+        << "\t First, you MUST make sure the same event is spelled the same EVERYWHERE, I will not spellcheck!" << endl
+        << "\t Second, to indicate a conditional probabiltiy, use the character | (this character may not be used anywhere else)" << endl;
+}
+
+void exampleHeader() {
+    cout << "This requires absolutly no user interaction beyond your choice of which example you would like to see.";
+}
---
+++ Utility.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/*
+ * Requires: Nothing
+ * Modifies: cout
+ * Effects:  Prints out the header for the calculator portion of the program.
+ */
+void calcHeader();
+
+/*
+* Requires: Nothing
+* Modifies: cout
+* Effects:  Prints out the header for the example portion of the calcualtor.
+*/
+void exampleHeader();
+
